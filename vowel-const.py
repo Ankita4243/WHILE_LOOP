602c46720c252f6b3ae4aa8c853f59386c0cbba4
Ankita4243 <91268920+Ankita4243@users.noreply.github.com>
2021-11-21 22:48:35 +0530

    Add files via upload

---
+++ vowel-const.py
@@ -0,0 +1,18 @@
+char=input("enter the alphabets:")
+if(char=="a" or char=="e" or char=="i" or char=="o" or char=="u" or 
+         char=="A" or char=="E" or char=="I" or char=="O" or char=="U"):
+    print("it's vowel")
+else:
+    print("it's constant")
+
+or....................
+
+char=input("enter the alphabets:")
+if(char=="a" or char=="e" or char=="i" or char=="o" or char=="u" or 
+         char=="A" or char=="E" or char=="I" or char=="O" or char=="U"):
+    print("it's vowel")
+elif (char>="A" and char<="Z" or char>="a" and "a"<="z"):
+    print("its consonat")
+else:
+    ("Enter the alphabets:")
+
